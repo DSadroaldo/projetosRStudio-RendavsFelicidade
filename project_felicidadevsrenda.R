d8b174e0c9ff74680a88aa8a5c661b437f343485
DSadroaldo <infotecnasp@gmail.com>
2025-09-08 18:26:45 +0000

    instalando e utilizando bibliotecas necessarias para manipulação do dataframe
    installing and using libraries necessary for manipulating the dataframe

---
+++ project_felicidadevsrenda.R
@@ -0,0 +1,24 @@
+#=========================================================================#
+#                         MODELAGEM ESTATÍSTICA                           #
+#                                AULA 2                                   #
+#-------------------------------------------------------------------------#
+#                                                                         #                                                   
+#-------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------
+# limpar variáveis
+rm(list=ls())
+#------------------------------------------------------------------------
+# Pacotes Necessários
+
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("broom")
+install.packages("ggpubr")
+install.packages("nima")
+
+library("ggplot2")
+library("dplyr")
+library("broom")
+library("ggpubr")
+library("nima")
